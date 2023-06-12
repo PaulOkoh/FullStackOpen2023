61f1e018fc9396dd51e70420aa421adbe7ea2207
Paul Okoh <pauldokoh@gmail.com>
2023-06-12 05:51:36 -0500

    add unicafe and courseinfo exercises

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [ counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const setToZero = () => setCounter(0)
+
+
+  return (
+    <div>
+     <div>{counter}</div>
+     <button onClick={increaseByOne}>
+      plus
+     </button>
+     <button onClick={setToZero}>
+      zero
+     </button>
+    </div>
+  )
+}
+export default App
+
+
+
+
+
+
+
+
+// const App = () => {
+//   const name = "Peter";
+//   const age = 10;
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} /> 
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// const Hello = ({ name, age}) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return(
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+
